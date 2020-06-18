594bfb38e680bd093ec29283069719b6cc47b31d
nrochard <nrochard@student.42.fr>
2020-06-18 21:17:02 +0200

    clean css files

--- helpers.php
+++ helpers.php
@@ -4,7 +4,7 @@
 function dbConnect()
 {
     try {
-        $db = new PDO('mysql:host=localhost:3306;dbname=dv19rochard;charset=utf8', 'dv19rochard', 'xT4@i7g7ovMtpuMy', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));//$e contiendra les éventuels messages d’erreur
+        $db = new PDO('mysql:host=localhost;dbname=icone;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     } catch (Exception $exception) //$e contiendra les éventuels messages d’erreur
     {
         die('Erreur : ' . $exception->getMessage());
